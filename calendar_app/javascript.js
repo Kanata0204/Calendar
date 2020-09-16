27757475a0333fa38e0136cea94515dc96c7a03b
kanata <kanahira24@outlook.jp>
2020-09-16 21:01:59 +0900

    complete calendar-ver1.0

--- calendar_app/javascript.js
+++ calendar_app/javascript.js
@@ -2,28 +2,59 @@ let date = new Date();
 
 let year = date.getFullYear();
 let month = date.getMonth() + 1;
-let day = date.getDate()
-
-console.log(year, month, day);
+let day = date.getDate();
 
 let table_title = year + "年" + month + "月";
 let captionHtml = "<caption>" + table_title + "</caption>";
-let weekdays = ["日", "月", "火", "水", "木", "金", "土"];
+let weekdaysAry = ["日", "月", "火", "水", "木", "金", "土"];
 let weekdayStr = "<tr>";
 
-for (var i = 0; i < 7; i++) {
+// 日～土までのHTMLが入る変数（WeekdayStr）を生成
+for (var i = 0; i < weekdaysAry.length; i++) {
     if (i == 0) {
-        weekdayStr += "<td>" + weekdays[i] + "</td>";
+        weekdayStr += "<td>" + weekdaysAry[i] + "</td>";
     } else if (i == 6) {
-        weekdayStr += "<td>" + weekdays[i] + "</td>";
+        weekdayStr += "<td>" + weekdaysAry[i] + "</td>";
     } else {
-        weekdayStr += "<td>" + weekdays[i] + "</td>";
+        weekdayStr += "<td>" + weekdaysAry[i] + "</td>";
+    }
+}
+weekdayStr += "</tr>";
+
+// WeekdayStr（日～土）を表示
+let calendar = document.querySelector("#calendar");
+calendar.innerHTML = captionHtml + weekdayStr;
+
+let firstDate = new Date(year, month - 1, 1);
+let firstWeekday = firstDate.getDay();
+let dayData = "";
+
+for (let i = 0; i < firstWeekday; i++) {
+    if (i === 0) {
+        dayData += "<tr>"
+    }
+    dayData += "<td>&nbsp;</td>";
+}
+
+// 翌月のDateを取得
+let lastDate = new Date(year, month, 0);
+
+// 今月の末日を取得
+let lastDay = lastDate.getDate();
+
+for (let i = 1; i < lastDay; i++) {
+    let calendarDate = new Date(year, month - 1, i);
+    let calendarWeekday = calendarDate.getDay();
+
+    if (calendarWeekday === 0) {
+        dayData += "<tr>"
+    }
+
+    dayData += "<td>" + i + "</td>";
+
+    if (calendarWeekday === weekdaysAry.length - 1) {
+        dayData += "</tr>";
     }
 }
 
-weekdayStr += "</tr>";
-
-// console.log(weekdayStr);
-
-let calendar = document.querySelector("#calendar");
-calendar.innerHTML = captionHtml + weekdayStr;
+calendar.innerHTML += dayData;
